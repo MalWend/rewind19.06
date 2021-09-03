f6e84120f9ae5ae30019dc965fb866970949ed94
Your name <Magorzata abendowicz>
2021-09-03 12:58:07 +0200

    new motorcycle

---
+++ src/Motorcycle.java
@@ -0,0 +1,15 @@
+public class Motorcycle {
+    private int price;
+    private String model;
+    private double capacity;
+    private int productionYear;
+    private char size;
+
+    public Motorcycle (int price, String model, double capacity, int productionYear, char size){
+        this.price = price;
+        this.model = model;
+        this.capacity = capacity;
+        this.productionYear = productionYear;
+        this.size = size;
+    }
+}
