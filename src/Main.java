02d7e88049881ad6f285313027485f77787c7ae3
Your name <Magorzata abendowicz>
2021-09-03 12:49:32 +0200

    tablica

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 3;
-        int y;
+        int[] tablica = {1,2,3,4,5,6};
+        System.out.println(tablica[2]);
 
-        System.out.println(x++);
-        System.out.println(++x);
-        System.out.println(x);
-        y = x++;
-        System.out.println(y);
-        y = ++x;
-        System.out.println(y);
 
     }
 }
